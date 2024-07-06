ee847c2dda6fe63cceecfc82cec966e034803bf5
Yongzhe Wang <w1625369372@gmail.com>
2024-07-06 20:40:02 +0800

    1

---
+++ Lab/lab00/lab00.py
@@ -0,0 +1,9 @@
+def twenty_twenty_four():
+    """Come up with the most creative expression that evaluates to 2024
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_four()
+    2024
+    """
+    a = (2020 + 2) * 2 - 2020 + (2024 - 2 * 1000) - 24
+    return a
